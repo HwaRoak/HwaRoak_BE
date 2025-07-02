9dc6235320f447391d13f5820d3f8289f040b27d
kite_U <yurry45@naver.com>
2025-07-02 17:07:47 +0900

    Initial Settings

--- src/main/java/com/umc/hwarok/HwaroakApplication.java
+++ src/main/java/com/umc/hwarok/HwaroakApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HwarokApplication {
+public class HwaroakApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HwarokApplication.class, args);
+        SpringApplication.run(HwaroakApplication.class, args);
     }
 
 }
