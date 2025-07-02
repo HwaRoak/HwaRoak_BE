95678467b4b9fe978e61e234e60d92b562ea7e83
kite_U <yurry45@naver.com>
2025-07-02 16:06:56 +0900

    Initial Settings

---
+++ src/test/java/com/umc/hwarok/HwarokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.hwarok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwarokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
