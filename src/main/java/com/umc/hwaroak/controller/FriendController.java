966cf43e4e47a798659068fff6ef307e435b1a1f
geniusjun <geniusjun4663@gmail.com>
2025-07-15 15:55:30 +0900

    feat: 스웨거에 에러표시 추가(#16)

--- src/main/java/com/umc/hwaroak/controller/FriendController.java
+++ src/main/java/com/umc/hwaroak/controller/FriendController.java
@@ -21,6 +21,7 @@ public class FriendController {
 
     @Operation(summary = "친구 요청 보내기", description = "상대방에게 친구 요청을 보냅니다.")
     @ApiResponse(responseCode = "200", description = "친구 요청 성공")
+    @ApiResponse(responseCode = "400", description = "존재하지 않는 사용자 (MEMBER_NOT_FOUND) 또는 자기 자신에게 요청 (CANNOT_ADD_SELF) 또는 중복 요청 (FRIEND_ALREADY_EXISTS_OR_REQUESTED)")
     @PostMapping("/request")
     public void requestFriend(@RequestBody FriendRequestDto.Request requestDto) {
         friendService.requestFriend(requestDto.getReceiverId());
@@ -28,6 +29,8 @@ public class FriendController {
 
     @Operation(summary = "친구 요청 수락", description = "상대방이 보낸 친구 요청을 수락합니다.")
     @ApiResponse(responseCode = "200", description = "친구 요청 수락 성공")
+    @ApiResponse(responseCode = "400", description = "이미 처리된 요청 (FRIEND_REQUEST_NOT_PENDING)")
+    @ApiResponse(responseCode = "404", description = "요청 보낸 사용자 없음 (MEMBER_NOT_FOUND) 또는 친구 요청 없음 (FRIEND_REQUEST_NOT_FOUND)")
     @PostMapping("/accept")
     public void acceptFriendRequest(@RequestBody FriendRequestDto.Accept requestDto) {
         friendService.acceptFriendRequest(requestDto.getSenderId());
@@ -35,6 +38,8 @@ public class FriendController {
 
     @Operation(summary = "친구 요청 거절", description = "상대방이 보낸 친구 요청을 거절합니다.")
     @ApiResponse(responseCode = "200", description = "친구 요청 거절 성공")
+    @ApiResponse(responseCode = "400", description = "이미 처리된 요청 (FRIEND_REQUEST_NOT_PENDING)")
+    @ApiResponse(responseCode = "404", description = "요청 보낸 사용자 없음 (MEMBER_NOT_FOUND) 또는 친구 요청 없음 (FRIEND_REQUEST_NOT_FOUND)")
     @PostMapping("/reject")
     public void rejectFriendRequest(@RequestBody FriendRequestDto.Reject requestDto) {
         friendService.rejectFriendRequest(requestDto.getSenderId());
@@ -46,5 +51,4 @@ public class FriendController {
     public List<FriendResponseDto.FriendInfo> getFriendList() {
         return friendService.getFriendList();
     }
-
 }
