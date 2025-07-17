39dbbbc7d8cb9c3fd83913f1189b6d4384fcafe2
yc3697 <youngchan9683@gmail.com>
2025-07-17 16:48:19 +0900

    chore: 스웨거 관련 어노테이션 추가 #14

--- src/main/java/com/umc/hwaroak/controller/MemberController.java
+++ src/main/java/com/umc/hwaroak/controller/MemberController.java
@@ -4,6 +4,10 @@ package com.umc.hwaroak.controller;
 import com.umc.hwaroak.dto.MemberResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.umc.hwaroak.service.MemberService;
 
+@Tag(name = "Member API", description = "사용자 관련 API")
 @RestController
-@RequestMapping("/members")
+@RequestMapping("api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -20,6 +25,7 @@ public class MemberController {
 
     @GetMapping("/{memberId}")
     @Operation(summary = "회원 정보 조회", description = "회원 정보를 조회합니다.(임시로 id 기반 조회)")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = MemberResponseDto.InfoDto.class)))
     public MemberResponseDto.InfoDto getInfo(
             @Parameter(name = "memberId", description = "회원 id", example = "1")
             @PathVariable Long memberId
