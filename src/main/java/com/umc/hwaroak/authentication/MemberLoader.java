eb58c4196f58e6fd7de2c10a4f8934403eb83c65
kite_U <yurry45@naver.com>
2025-07-18 23:38:10 +0900

    fix: SpringContext 기반 Member 반환 (#24)

---
+++ src/main/java/com/umc/hwaroak/authentication/MemberLoader.java
@@ -0,0 +1,45 @@
+package com.umc.hwaroak.authentication;
+
+import com.umc.hwaroak.domain.Member;
+import com.umc.hwaroak.exception.GeneralException;
+import com.umc.hwaroak.repository.MemberRepository;
+import com.umc.hwaroak.response.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberLoader {
+
+    private final MemberRepository memberRepository;
+
+    public Long getCurrentMemberId() {
+        return getMemberByContextHolder().getId();
+    }
+
+    public Member getMemberByContextHolder() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof UsernamePasswordAuthenticationToken)) {
+            throw new GeneralException(ErrorCode.UNAUTHORIZED_ACCESS);
+        }
+
+        Object principal = authentication.getPrincipal();
+        Long memberId;
+
+        if (principal instanceof Long) {
+            memberId = (Long) principal;
+        } else if (principal instanceof String) {
+            memberId = Long.parseLong((String) principal);
+        } else {
+            throw new GeneralException(ErrorCode.UNAUTHORIZED_ACCESS);
+        }
+
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+}
