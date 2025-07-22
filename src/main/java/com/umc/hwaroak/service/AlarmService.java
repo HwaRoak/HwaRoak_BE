8d0020b6adce0d4b7abaa34bc8a7f486007d9247
kite_U <yurry45@naver.com>
2025-07-23 03:26:39 +0900

    chore: 주석 추가 (#36)

--- src/main/java/com/umc/hwaroak/service/AlarmService.java
+++ src/main/java/com/umc/hwaroak/service/AlarmService.java
@@ -32,6 +32,7 @@ public interface AlarmService {
      */
     void markAsRead(Long alarmId, Member member);
 
+    /**
      *  공지 수동 등록
      */
     void createNotice(AlarmRequestDto.CreateNoticeDto requestDto);
