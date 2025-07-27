96258243d0ba69b54da30c5440ff867dbb575e2b
kite_U <yurry45@naver.com>
2025-07-27 19:56:32 +0900

    feat: 알람 DTO 필드에 정보 추가 (#61)

--- src/main/java/com/umc/hwaroak/dto/response/AlarmResponseDto.java
+++ src/main/java/com/umc/hwaroak/dto/response/AlarmResponseDto.java
@@ -1,5 +1,6 @@
 package com.umc.hwaroak.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.umc.hwaroak.domain.common.AlarmType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -19,13 +20,23 @@ public class AlarmResponseDto {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class PreviewDto {
 
         @Schema(description = "알람 ID", example = "1")
         private Long id;
 
+        @Schema(description = "수신자 ID", example = "1")
+        private Long receiverId;
+
         @Schema(description = "알람 제목", example = "서버 점검 안내")
         private String title;
+
+        @Schema(description = "알람 타입")
+        private AlarmType alarmType;
+
+        @Schema(description = "알람 메세지", example = "서버 점검 공지를 확인해보세요.")
+        private String message;
 
         @Schema(description = "알람 생성일")
         private LocalDateTime createdAt; // 프론트에서 언제 올라온 공지인지 정렬 및 표시용
@@ -49,7 +60,7 @@ public class AlarmResponseDto {
         @Schema(description = "공지 내용", example = "OO님이 친구요청을 보냈습니다.")
         private String content;
 
-        @Schema(description = "알람 종류", example = "친구요청")
+        @Schema(description = "알람 종류", example = "FRIEND_REQUEST")
         private AlarmType alarmType;
 
         @Schema(description = "알람 생성일")
