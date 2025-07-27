926c72305ed745618352f3e1cfa90f8e5a14ca22
kite_U <yurry45@naver.com>
2025-07-27 12:29:37 +0900

    refactor: AlarmController MemberLoader 의존성 제거 (#61)

--- src/main/java/com/umc/hwaroak/service/AlarmService.java
+++ src/main/java/com/umc/hwaroak/service/AlarmService.java
@@ -25,13 +25,14 @@ public interface AlarmService {
     /**
      * 로그인한 유저의 알림함 전체를 최신순으로 조회
      */
-    List<AlarmResponseDto.InfoDto> getAllAlarmsForMember(Member member);
+    List<AlarmResponseDto.InfoDto> getAllAlarmsForMember();
 
     /**
      *  알람 읽음 처리 하기
      */
-    void markAsRead(Long alarmId, Member member);
+    void markAsRead(Long alarmId);
 
+    /**
      *  공지 수동 등록
      */
     void createNotice(AlarmRequestDto.CreateNoticeDto requestDto);
