1a56ea13d16144c185e7de079656bdf100f5245b
kite_U <yurry45@naver.com>
2025-08-05 23:38:46 +0900

    fix: JPA LOCK 어노테이션 주석 처리 (#85)

--- src/main/java/com/umc/hwaroak/repository/DiaryRepository.java
+++ src/main/java/com/umc/hwaroak/repository/DiaryRepository.java
@@ -3,17 +3,18 @@ package com.umc.hwaroak.repository;
 import com.umc.hwaroak.domain.Diary;
 import com.umc.hwaroak.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long>, DiaryRepositoryCustom {
 
+    //@Lock(LockModeType.PESSIMISTIC_WRITE) // 잠금
     @Query("SELECT d FROM Diary d WHERE d.recordDate = :recordDate and d.member.id = :memberId")
     Optional<Diary> findByRecordDate(@Param("memberId") Long memberId, @Param("recordDate") LocalDate recordDate);
 
@@ -22,7 +23,5 @@ public interface DiaryRepository extends JpaRepository<Diary, Long>, DiaryReposi
 
     //"최근 3일 내 작성된 다이어리 중 가장 최신 것 하나"만 가져오기
     Optional<Diary> findTop1ByMemberAndRecordDateGreaterThanEqualOrderByRecordDateDesc(Member member, LocalDate date);
-
-
 
 }
