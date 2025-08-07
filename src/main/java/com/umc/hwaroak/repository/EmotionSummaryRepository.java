fb9ff7710d388ef5a5aead71c6f67ec1808592b3
yc3697 <youngchan9683@gmail.com>
2025-08-07 14:56:32 +0900

    feat: 감성분석 자동 삭제 기능 구현 #98

--- src/main/java/com/umc/hwaroak/repository/EmotionSummaryRepository.java
+++ src/main/java/com/umc/hwaroak/repository/EmotionSummaryRepository.java
@@ -2,6 +2,10 @@ package com.umc.hwaroak.repository;
 
 import com.umc.hwaroak.domain.EmotionSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -9,4 +13,12 @@ public interface EmotionSummaryRepository extends JpaRepository<EmotionSummary,L
 
     // 멤버ID, 연도,월로 분석 데이터 조회
     Optional<EmotionSummary> findByMemberIdAndSummaryMonth(Long memberId, String summaryMonth);
+
+    /**
+     * @param date 감정분석 데이터 삭제할 기준 일자
+     */
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM EmotionSummary es WHERE es.summaryMonth <= :date")
+    void deleteEmotionSummaryBefore(@Param("date") String date);
 }
