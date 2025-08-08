424b05361f3bc87269e0317de02bb8fc4ed82848
yenalee <lyemee@naver.com>
2025-08-08 11:41:33 +0900

    refactor: swagger description fix(#97)

--- src/main/java/com/umc/hwaroak/controller/AlarmController.java
+++ src/main/java/com/umc/hwaroak/controller/AlarmController.java
@@ -90,7 +90,7 @@ public class AlarmController {
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @Operation(summary = "공지 등록", description = "관리자가 공지를 수동 등록합니다.")
+    @Operation(summary = "공지 등록", description = "관리자만 공지를 등록할 수 있습니다.")
     @ApiResponse(responseCode = "201", description = "공지 등록 성공")
     @ApiResponse(responseCode = "4032", description = "접근 권한 없음 (ADMIN_ACCESS_ONLY)")
     @PostMapping("/notices")
