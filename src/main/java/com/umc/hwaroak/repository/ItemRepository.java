d1d828997bc3335e61f3c1ba418a1fade01e3c60
kite_U <yurry45@naver.com>
2025-08-15 14:49:31 +0900

    del: 주석 삭제 (#123)

--- src/main/java/com/umc/hwaroak/repository/ItemRepository.java
+++ src/main/java/com/umc/hwaroak/repository/ItemRepository.java
@@ -3,7 +3,6 @@ package com.umc.hwaroak.repository;
 import com.umc.hwaroak.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
