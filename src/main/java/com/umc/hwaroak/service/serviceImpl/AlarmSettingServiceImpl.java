5726f9ee31b154a16d1326e631bde900e8938428
kite_U <yurry45@naver.com>
2025-08-22 05:35:59 +0900

    hotfix: 알람 중복 생성 오류 수정

--- src/main/java/com/umc/hwaroak/service/serviceImpl/AlarmSettingServiceImpl.java
+++ src/main/java/com/umc/hwaroak/service/serviceImpl/AlarmSettingServiceImpl.java
@@ -62,20 +62,20 @@ public class AlarmSettingServiceImpl implements AlarmSettingService {
                     return alarmSettingRepository.save(defaultSetting);
                 });
 
-        Alarm alarm = alarmRepository.findByMemberIdAndAlarmType(memberId)
-                .orElseGet(() -> {
-                    log.info("알람이 존재하지 않아 새로 생성 중... {}", memberId);
-                    Alarm defaultAlarm = Alarm.builder()
-                            .alarmType(AlarmType.REMINDER)
-                            .receiver(member)
-                            .title("오늘의 이야기로 화록을 불태우세요!")
-                            .content("오늘 하루는 어땠나요? 저에게 들려주세요!")
-                            .message("오늘 하루는 어땠나요? 저에게 들려주세요!")
-                            .build();
-                    alarmRepository.save(defaultAlarm);
-                    return defaultAlarm;
-                });
-        reminderTaskScheduler.addSchedule(alarm);
+        if (setting.isReminderEnabled()) {
+            if (!alarmRepository.findByMemberIdAndAlarmType(memberId).isPresent()) {
+                log.info("알람이 존재하지 않아 새로 생성 중... {}", memberId);
+                Alarm defaultAlarm = Alarm.builder()
+                        .alarmType(AlarmType.REMINDER)
+                        .receiver(member)
+                        .title("오늘의 이야기로 화록을 불태우세요!")
+                        .content("오늘 하루는 어땠나요? 저에게 들려주세요!")
+                        .message("오늘 하루는 어땠나요? 저에게 들려주세요!")
+                        .build();
+                alarmRepository.save(defaultAlarm);
+                reminderTaskScheduler.addSchedule(defaultAlarm);
+            }
+        }
 
         return AlarmSettingResponseDto.InfoDto.builder()
                 .reminderEnabled(setting.isReminderEnabled())
@@ -115,6 +115,7 @@ public class AlarmSettingServiceImpl implements AlarmSettingService {
 
         Alarm alarm = alarmRepository.findByMemberIdAndAlarmType(memberId)
                 .orElseThrow(() -> new GeneralException(ErrorCode.ALARM_NOT_FOUND));
+        log.debug("알람 정보 확인... {}", memberId);
 
         if (!setting.isReminderEnabled()) { // 알람 설정 off
             reminderTaskScheduler.cancel(memberId);
